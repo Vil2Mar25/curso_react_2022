6707a0e2f6eed69fce4a856fc183dae28f22ed46
“Vilmar” <frvilmar@hotmail.com>
2022-10-24 20:47:31 -0300

    exercicio - cafe da manha

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lista de compras</h1>
+        <ul>
+          <li>Pão</li>
+          <li>Banana</li>
+          <li>Milk Shake de Doce de Leite</li>
+          <li>Nutella</li>
+          <li>Sorvete</li>
+        </ul>
+      </div>
+     
+    );
+  }
+}
+
+export default App;
